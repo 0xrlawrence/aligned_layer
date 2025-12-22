3aeb64a76c6ffc70ba361070db1accf6bc6cc059
maximopalopoli <mpalopoli@fi.uba.ar>
2025-12-22 15:29:01 -0300

    use tracing::error instead of eprintln

--- aggregation_mode/gateway/src/metrics.rs
+++ aggregation_mode/gateway/src/metrics.rs
@@ -38,7 +38,7 @@ impl GatewayMetrics {
 
         let mut buffer = Vec::new();
         if let Err(e) = encoder.encode(&registry.gather(), &mut buffer) {
-            eprintln!("could not encode prometheus metrics: {e}");
+            tracing::error!("could not encode prometheus metrics: {e}");
         };
         let res = String::from_utf8(buffer.clone())
             .inspect_err(|e| eprintln!("prometheus metrics could not be parsed correctly: {e}"))
